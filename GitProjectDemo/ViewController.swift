078dd4bb543594926ff675e2f1c7507e56843b02
sumathi <sumathilode@gmail.com>
2021-04-18 20:05:57 +0530

    updating vc

--- GitProjectDemo/ViewController.swift
+++ GitProjectDemo/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("Main:Updating main branch")
     }
     func ex(){
         
