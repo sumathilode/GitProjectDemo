57560597277c9b4dd56c43a1977149e6f20a0e12
sumathi <sumathilode@gmail.com>
2021-04-18 19:42:52 +0530

    first commit

---
+++ GitProjectDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitProjectDemo
+//
+//  Created by Sumathi Lode on 18/04/21.
+//  Copyright © 2021 sumathi lode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
