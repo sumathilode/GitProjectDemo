e7440791a4c1328adeaddb0de92cb9d8ca10204a
sumathi <sumathilode@gmail.com>
2021-04-18 20:15:06 +0530

    no message

--- GitProjectDemo/ViewController.swift
+++ GitProjectDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Main:Updating main branch")
+        print("Hello world")
     }
 
 
