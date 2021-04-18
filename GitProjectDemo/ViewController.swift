275e8291b455cffd146d5fa6cce5e1588a34d71a
sumathi <sumathilode@gmail.com>
2021-04-18 20:24:48 +0530

    merge

--- GitProjectDemo/ViewController.swift
+++ GitProjectDemo/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("Main:Updating main branch")
         print("Hello world")
+        print("merge")
     }
     func ex(){
         
